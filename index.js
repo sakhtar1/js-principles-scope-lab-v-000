c5597a4dc330fe033adddfc6d708bc1116e1b55b
Sharmina Akhtar <sakht318@gmail.com>
2018-05-25 07:58:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = 'bob'
 
 function upperCaseCustomerName(){
-  return customerName.toUpperCase();
+  customerName.toUpperCase();
 }
 
 function setBestCustomer() {
@@ -11,4 +11,8 @@ function setBestCustomer() {
 
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer =
 }
